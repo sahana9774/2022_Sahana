a0146df72c0e5e44451810ab093c53dd9429fcfe
Kasia Kedziora <7549583+fjorka@users.noreply.github.com>
2022-11-30 14:40:38 -0500

    characterization of objects

--- code/cardiomyocytes_helper_functions.py
+++ code/cardiomyocytes_helper_functions.py
@@ -5,7 +5,12 @@ from aicssegmentation.core.vessel import filament_3d_wrapper
 from aicssegmentation.core.pre_processing_utils import intensity_normalization, edge_preserving_smoothing_3d, image_smoothing_gaussian_3d
 from skimage.morphology import remove_small_objects  
 from skimage.draw import polygon
+from skimage.transform import probabilistic_hough_line
+from skimage.morphology import skeletonize
+from skimage.measure import profile_line
+import math
 
+################################################################################################################################
 # create a mask out of vertices
 def create_mask_from_shapes(vertices_polygons, im_shape):
 
@@ -31,7 +36,8 @@ def create_mask_from_shapes(vertices_polygons, im_shape):
 
     return mask
 
-
+################################################################################################################################
+# segmentation of actin in 3D volumes
 def segment_actin_3D(image_actin):
 
     '''
@@ -64,6 +70,7 @@ def segment_actin_3D(image_actin):
     return image_actin_mask
 
 
+################################################################################################################################
 def find_fibers_orientation(image_actin_mask_2D):
 
     '''
@@ -84,3 +91,91 @@ def find_fibers_orientation(image_actin_mask_2D):
     dominant_flow = np.mean(angle_array[:4])
 
     return dominant_flow
+
+################################################################################################################################
+def calculate_orientation(p0,p1):
+    
+    myrad = -(math.atan2(p1[1]-p0[1], p1[0]-p0[0]) + np.pi/2)
+    
+    return myrad
+
+################################################################################################################################
+def find_fibers_orientation_v2(actin_im):
+
+    # skeletonize the image
+    skeleton_im = skeletonize(actin_im)
+
+    # find straight lines in the image
+    lines = probabilistic_hough_line(skeleton_im, threshold=4, line_length=15,
+                                    line_gap=2)
+
+    # calculate orientation of the lines
+    rad_list = []
+    for line in lines:
+
+        p0, p1 = line
+        actin_rad = calculate_orientation(p0,p1)
+
+        rad_list.append(actin_rad)
+
+    # calculate mean orientation of the fibers
+    mean_orientation = np.mean(rad_list)
+
+    # calculate spread of orientations
+    std_orientation = np.std(rad_list)
+
+    return lines,mean_orientation,std_orientation
+
+
+################################################################################################################################
+
+def orientations_from_vertices(vert):
+
+    '''
+    It accepts vertices in the format given by Napari
+    Returns the list of orientations of membrane segments in the style of scikit.image orientation (-pi/2 to pi/2)
+    '''
+
+    my_rad_list = []
+
+    for i in range(len(vert)):
+
+        p0 = vert[i,:]
+        
+        if i == (len(vert)-1):
+            p1 = vert[0,:]
+        else:
+            p1= vert[i+1,:]
+
+        my_rad = -(calculate_orientation(p1,p0) % np.pi - np.pi/2)
+
+        my_rad_list.append(my_rad)
+
+    return my_rad_list
+
+    ################################################################################################################################
+
+def signal_from_vertices(vert,signal_im):
+
+    '''
+    It accepts vertices in the format given by Napari
+    '''
+
+    signal_line = []
+
+    for i in range(len(vert)):
+
+        p0 = vert[i,:]
+        
+        if i == (len(vert)-1):
+            p1 = vert[0,:]
+        else:
+            p1= vert[i+1,:]
+
+        signal_segment = profile_line(signal_im,p0,p1)
+
+        signal_line.extend(signal_segment)
+
+    return signal_line
+
+        
